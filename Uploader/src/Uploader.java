c82b64ebae9e40ccc4c4ba7ed8bdd1f182d2ce07
Noah Hölterhoff <noah.hoelterhoff@gmail.com>
2020-09-20 20:43:54 +0200

    added service restart to Uploader

--- Uploader/src/Uploader.java
+++ Uploader/src/Uploader.java
@@ -6,7 +6,9 @@
  */
 
 import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.connection.ConnectionException;
 import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
 import net.schmizz.sshj.xfer.FileSystemFile;
 
@@ -20,8 +22,27 @@ import java.io.IOException;
  */
 public abstract class Uploader {
     private static String hostname, username, password, pathExternal, pathLocal;
+    private static SSHClient ssh;
+
+    private static void chnageService(String function) throws ConnectionException, TransportException {
+        ssh.startSession().exec("sudo systemctl " + function + " cinema");
+        System.out.println("Service: " + function);
+    }
+
+    private static void connect() throws IOException {
+        // Connect
+        ssh = new SSHClient();
+        ssh.addHostKeyVerifier(new PromiscuousVerifier());
+        ssh.connect(hostname);
+        System.out.println("Connected");
+
+        // Authorize
+        ssh.authPassword(username, password);
+        System.out.println("Authorized");
+    }
 
     public static void main(String[] args) throws IOException {
+        // read args
         for (String s : args) {
             if (s.startsWith("HOSTNAME=")) hostname = s.substring(9);
             if (s.startsWith("USERNAME=")) username = s.substring(9);
@@ -29,25 +50,23 @@ public abstract class Uploader {
             if (s.startsWith("PATHE=")) pathExternal = s.substring(6);
             if (s.startsWith("PATHL=")) pathLocal = s.substring(6);
         }
-        upload();
+
+        // execute
+        try {
+            connect();
+            chnageService("stop");
+            upload();
+        } finally {
+            chnageService("start");
+        }
     }
 
-    public static void upload() throws IOException {
-        SSHClient ssh = new SSHClient();
-        ssh.addHostKeyVerifier(new PromiscuousVerifier());
-        ssh.connect(hostname);
-        System.out.println("Connected");
-        try {
-            ssh.authPassword(username, password);
-            System.out.println("Authorized");
-            SFTPClient sftp = ssh.newSFTPClient();
-            System.out.println("Starting upload...");
-            sftp.put(new FileSystemFile(pathLocal), (pathExternal.endsWith("/") ? pathExternal +
-                    new File(pathLocal).getName() : pathExternal + "/" + new File(pathLocal).getName()));
-            System.out.println("Upload finished");
-            sftp.close();
-        } finally {
-            ssh.disconnect();
-        }
+    private static void upload() throws IOException {
+        SFTPClient sftp = ssh.newSFTPClient();
+        System.out.println("Starting upload...");
+        sftp.put(new FileSystemFile(pathLocal), (pathExternal.endsWith("/") ? pathExternal +
+                new File(pathLocal).getName() : pathExternal + "/" + new File(pathLocal).getName()));
+        System.out.println("Upload finished");
+        sftp.close();
     }
 }
