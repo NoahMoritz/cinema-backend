84a21efa8c14d2037554795b593ace70c1dc3a9e
NoahMoritz <noah.hoelterhoff@gmail.com>
2020-11-13 11:19:32 +0100

    tests

--- test/de/noamo/cinema/backend/DataBaseTest.java
+++ test/de/noamo/cinema/backend/DataBaseTest.java
@@ -7,8 +7,8 @@
 
 package de.noamo.cinema.backend;
 
-import de.noamo.cinema.backend.exceptions.BadRequestException;
-import de.noamo.cinema.backend.exceptions.ConflictException;
+import com.google.gson.JsonObject;
+import de.noamo.cinema.backend.exceptions.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -33,18 +33,65 @@ class DataBaseTest {
     @Test
     void createUser() throws BadRequestException, ConflictException, SQLException {
         Connection connection = DataBase.getConnection();
-        connection.prepareStatement("DELETE FROM aktivierungsSchluessel").executeUpdate();
-        connection.prepareStatement("DELETE FROM konten WHERE email='" + "test1@noamo.de" + "'").executeUpdate();
+        reset(connection);
 
         String code = DataBase.createUser("password1", "test1@noamo.de", "Test User", false);
-        Assertions.assertEquals(code.length(), 36);
+        Assertions.assertEquals(36, code.length());
         ResultSet rs = connection.prepareStatement("SELECT * FROM konten WHERE email='test1@noamo.de';").executeQuery();
         Assertions.assertTrue(rs.next());
-        Assertions.assertEquals(rs.getInt("aktiv"), 0);
+        Assertions.assertEquals(0, rs.getInt("aktiv"));
         ResultSet rs2 = connection.prepareStatement("SELECT * FROM aktivierungsSchluessel WHERE benutzerid=" + rs.getInt("benutzerid")).executeQuery();
         Assertions.assertTrue(rs2.next());
 
         connection.prepareStatement("DELETE FROM aktivierungsSchluessel").executeUpdate();
+        connection.prepareStatement("DELETE FROM konten WHERE email='" + "test1@noamo.de" + "'").executeUpdate();
+    }
+
+    @Test
+    void activateAccount() throws SQLException, BadRequestException, NotFoundException, ConflictException {
+        Connection connection = DataBase.getConnection();
+        reset(connection);
+
+        String code = DataBase.createUser("password1", "test1@noamo.de", "Test User", false);
+        DataBase.activateAccount(code);
+
+        ResultSet rs1 = connection.prepareStatement("SELECT * FROM aktivierungsSchluessel;").executeQuery();
+        Assertions.assertFalse(rs1.next());
+
+        ResultSet rs2 = connection.prepareStatement("SELECT * FROM konten WHERE email='" + "test1@noamo.de" + "'").executeQuery();
+        Assertions.assertTrue(rs2.next());
+        Assertions.assertEquals(1, rs2.getInt("aktiv"));
+
+        connection.prepareStatement("DELETE FROM aktivierungsSchluessel").executeUpdate();
+        connection.prepareStatement("DELETE FROM konten WHERE email='" + "test1@noamo.de" + "'").executeUpdate();
+    }
+
+    @Test
+    void kontoDeaktivieren() throws SQLException, BadRequestException, ConflictException, NotActiveException, UnauthorisedException, NotFoundException {
+        Connection connection = DataBase.getConnection();
+        reset(connection);
+
+        connection.prepareStatement("DELETE FROM konten WHERE email='" + "test1@noamo.de" + "'").executeUpdate();
+        DataBase.createUser("password1", "test1@noamo.de", "Test User", true);
+
+        ResultSet rs1 = connection.prepareStatement("SELECT * FROM konten WHERE email='test1@noamo.de';").executeQuery();
+        Assertions.assertTrue(rs1.next());
+
+        JsonObject json = new JsonObject();
+        json.addProperty("email", "test1@noamo.de");
+        json.addProperty("passwort", "password1");
+        JsonObject authJson = DataBase.login(json);
+        String auth = authJson.get("authToken").getAsString();
+
+        DataBase.kontoDeaktivieren(auth, "password1");
+        ResultSet rs2 = connection.prepareStatement("SELECT * FROM konten WHERE email='test1@noamo.de';").executeQuery();
+        Assertions.assertTrue(rs2.next());
+        Assertions.assertEquals(2, rs2.getInt("aktiv"));
+    }
+
+    private void reset(Connection connection) throws SQLException {
+        connection.prepareStatement("DELETE FROM aktivierungsSchluessel").executeUpdate();
+        connection.prepareStatement("DELETE FROM authCodes").executeUpdate();
         connection.prepareStatement("DELETE FROM konten WHERE email='" + "test1@noamo.de" + "'").executeUpdate();
     }
 }
